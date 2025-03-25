e8d682267bcde9684647a19c92e1e905736ecd72
Marcel Müller <neikos@neikos.email>
2025-03-25 11:15:19 +0100

    Initial Commit
    Signed-off-by: Marcel Müller <neikos@neikos.email>

---
+++ flake.nix
@@ -0,0 +1,88 @@
+{
+  description = "The rust-dsl library";
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-24.11";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
+    crane = {
+      url = "github:ipetkov/crane";
+    };
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+  };
+
+  outputs =
+    inputs:
+    inputs.flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import inputs.nixpkgs {
+          inherit system;
+          overlays = [ (import inputs.rust-overlay) ];
+        };
+
+        rustTarget = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+        unstableRustTarget = pkgs.rust-bin.selectLatestNightlyWith (
+          toolchain:
+          toolchain.default.override {
+            extensions = [
+              "rust-src"
+              "miri"
+              "rustfmt"
+            ];
+          }
+        );
+        craneLib = (inputs.crane.mkLib pkgs).overrideToolchain rustTarget;
+        unstableCraneLib = (inputs.crane.mkLib pkgs).overrideToolchain unstableRustTarget;
+
+        tomlInfo = craneLib.crateNameFromCargoToml { cargoToml = ./Cargo.toml; };
+        inherit (tomlInfo) version;
+        src = ./.;
+
+        rustfmt' = pkgs.writeShellScriptBin "rustfmt" ''
+          exec "${unstableRustTarget}/bin/rustfmt" "$@"
+        '';
+
+        cargoArtifacts = craneLib.buildDepsOnly {
+          inherit src;
+          cargoExtraArgs = "--all-features --all";
+        };
+
+        crate = craneLib.buildPackage {
+          inherit cargoArtifacts src version;
+          cargoExtraArgs = "--all-features --all";
+        };
+
+      in
+      rec {
+        checks = {
+          inherit crate;
+
+          crate-clippy = craneLib.cargoClippy {
+            inherit cargoArtifacts src;
+            cargoExtraArgs = "--all --all-features";
+            cargoClippyExtraArgs = "-- --deny warnings";
+          };
+
+          crate-fmt = unstableCraneLib.cargoFmt {
+            inherit src;
+          };
+        };
+
+        devShells.default = devShells.crate;
+        devShells.crate = pkgs.mkShell {
+          buildInputs = [ ];
+
+          nativeBuildInputs = [
+            rustfmt'
+            rustTarget
+          ];
+        };
+      }
+    );
+}
